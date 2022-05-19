b1919cd6a14e4e0610cd1c449e1e8d6f17934436
LAKSHMAN KUMAR <lakshmankumarkalasamudram@gmail.com>
2022-05-19 20:51:37 +0530

    deleted CSVFileReader useless class

--- IplDataAnalysis.java
+++ IplDataAnalysis.java
@@ -5,7 +5,7 @@ public class IplDataAnalysis {
     public static void main(String[] args) throws IOException {
         List<MatchDetails> md=MatchDetails.getMatchDetailsList("/home/lakshman/IdeaProjects/IPLProject/Resources/matches.csv");
         List<DeliveriesDetails> dd=DeliveriesDetails.getDeliveriesDetailsList("/home/lakshman/IdeaProjects/IPLProject/Resources/deliveries.csv");
-        
+
 
     }
 }
