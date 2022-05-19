492f38e7253cf4ba62ab66a7850fe4978dd41aea
LAKSHMAN KUMAR <lakshmankumarkalasamudram@gmail.com>
2022-05-19 16:47:54 +0530

    Shifted the logic of CSVFileStream class into MatchDetails class

--- IplDataAnalysis.java
+++ IplDataAnalysis.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.util.List;
+
 public class IplDataAnalysis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        List<MatchDetails> md=MatchDetails.getMatchDetailsList("/home/lakshman/IdeaProjects/IPLProject/Resources/matches.csv");
+        System.out.println(md);
 
     }
 }
--- MatchDetails.java
+++ MatchDetails.java
@@ -1,4 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MatchDetails {
+
+   static List<MatchDetails> matchDetailsList;
     //This is a POJO class
     //Teclaring all the fields of csv file as data members
     private final int id;
@@ -139,5 +147,26 @@ public class MatchDetails {
                 ", umpire2='" + umpire2 + '\'' +
                 ", umpire3='" + umpire3 + '\'' +
                 '}';
+    }
+
+
+    public static List<MatchDetails> getMatchDetailsList(String p) throws IOException {
+        Path path=Path.of(p);
+        //Below code throws IOException and returns a stream of string
+        //skip(1) will skip the first line because its a header
+        //map individual string to respective object
+        matchDetailsList= Files.lines(path).skip(1).map(line -> {return getMatchDetails(line);}).collect(Collectors.toList());
+        return matchDetailsList;
+    }
+
+    //Converting line into Object and returning
+    private static MatchDetails getMatchDetails(String line){
+        String[] fields=new String[18];
+        String[] acfields=line.split(",");
+        for(int i=0;i<acfields.length;i++){
+            fields[i]=acfields[i];
+        }
+        return new MatchDetails(Integer.parseInt(fields[0]),Integer.parseInt(fields[1]),fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],Integer.parseInt(fields[9]),
+                fields[10],fields[11],fields[12],fields[13],fields[14],fields[15],fields[16],fields[17]);
     }
 }
