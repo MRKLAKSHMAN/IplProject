9a66eb19bf7a2c81619a1021ab7696befcf24483
LAKSHMAN KUMAR <lakshmankumarkalasamudram@gmail.com>
2022-05-19 14:21:19 +0530

    declared all the fields

--- MatchDetails.java
+++ MatchDetails.java
@@ -1,2 +1,27 @@
 public class MatchDetails {
+    //This is a POJO class
+    //Teclaring all the fields of csv file as data members
+    private final int id;
+    private final int season;
+    private final String city;
+    private final String date;
+    private final String team1;
+    private final String team2;
+    private final String tosswinner;
+    private final String tossdecision;
+    private final String result;
+    private final int dlapplied;
+    private final String winner;
+    private final String winbyruns;
+    private final String winbywickets;
+    private final String playerofmatch;
+    private final String venue;
+    private final String umpire1;
+    private final String umpire2;
+    private final String umpire3;
+
+
+
+
+
 }
