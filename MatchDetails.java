f3c2c09cf4069f05f6537fddbd5f86abca046e96
LAKSHMAN KUMAR <lakshmankumarkalasamudram@gmail.com>
2022-05-19 14:24:21 +0530

    Initialized variables using constructor

--- MatchDetails.java
+++ MatchDetails.java
@@ -20,8 +20,25 @@ public class MatchDetails {
     private final String umpire2;
     private final String umpire3;
 
-
-
-
-
+    //Initializing all the fields using constructor
+    public MatchDetails(int id, int season, String city, String date, String team1, String team2, String tosswinner, String tossdecision, String result, int dlapplied, String winner, String winbyruns, String winbywickets, String playerofmatch, String venue, String umpire1, String umpire2, String umpire3) {
+        this.id = id;
+        this.season = season;
+        this.city = city;
+        this.date = date;
+        this.team1 = team1;
+        this.team2 = team2;
+        this.tosswinner = tosswinner;
+        this.tossdecision = tossdecision;
+        this.result = result;
+        this.dlapplied = dlapplied;
+        this.winner = winner;
+        this.winbyruns = winbyruns;
+        this.winbywickets = winbywickets;
+        this.playerofmatch = playerofmatch;
+        this.venue = venue;
+        this.umpire1 = umpire1;
+        this.umpire2 = umpire2;
+        this.umpire3 = umpire3;
+    }
 }
